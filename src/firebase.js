62a624c9bd4d20c4d2e6745ce9e9b0026cc8c382
aaronli39 <aaronli39@gmail.com>
2020-09-08 22:05:28 -0400

    cleaned up, added firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD26VefLcZdXwYqeObsbwlneMs3UzOP5no",
+    authDomain: "zipshop-7ff9f.firebaseapp.com",
+    databaseURL: "https://zipshop-7ff9f.firebaseio.com",
+    projectId: "zipshop-7ff9f",
+    storageBucket: "zipshop-7ff9f.appspot.com",
+    messagingSenderId: "741798167623",
+    appId: "1:741798167623:web:6d6318e20e436f84555d62",
+    measurementId: "G-MRC281RTB5"
+};
